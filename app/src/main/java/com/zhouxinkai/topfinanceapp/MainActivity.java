d15ebe63998a1f77866bea6a7d6927c984edb602
zxkan <810183409@qq.com>
2016-02-29 16:39:38 +0800

    将MainActivity中onResume()方法的注释去掉了

--- app/src/main/java/com/zhouxinkai/topfinanceapp/MainActivity.java
+++ app/src/main/java/com/zhouxinkai/topfinanceapp/MainActivity.java
@@ -71,10 +71,6 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    /**
-     * 这里是resume方法
-     * 新添加了一小段注释
-     */
     @Override
     protected void onResume() {
         super.onResume();
